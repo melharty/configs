1d1bbc0363056493c97adbc6ce0a093494bd3556
Mohamed El Harty <mohamed.elharty@nocsmart.com>
2016-12-08 13:31:05 +0200

    quick fix

--- vim-install.sh
+++ vim-install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
+wget https://raw.githubusercontent.com/melharty/configs/master/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
 vim +PluginInstall +qall
