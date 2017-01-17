071a82e51075e8aac4d03e239b96fc016da03d7d
Mohamed El Harty <mohamed.elharty@nocsmart.com>
2017-01-17 13:06:53 +0200

    Modified vim-install.sh

--- vim-install.sh
+++ vim-install.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 wget https://raw.githubusercontent.com/melharty/configs/master/.vimrc
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
